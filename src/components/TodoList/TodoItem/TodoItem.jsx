7fbeb887504a72f2385e171689806313e6a572ca
filmbronte <122818201+filmbronte@users.noreply.github.com>
2023-09-13 01:37:18 +0300

    add strikethrough on checked items

--- src/components/TodoList/TodoItem/TodoItem.jsx
+++ src/components/TodoList/TodoItem/TodoItem.jsx
@@ -1,14 +1,12 @@
 export const TodoItem = ({ id, completed, title, toggleTodo, deleteTodo }) => {
   return (
     <li>
-      <label>
-        <input
-          type="checkbox"
-          checked={completed}
-          onChange={(e) => toggleTodo(id, e.target.checked)}
-        />
-        {title}
-      </label>
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
+      <label className="test">{title}</label>
       <button onClick={() => deleteTodo(id)} className="btn btn-delete">
         Delete
       </button>
